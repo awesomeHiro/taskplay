93df80187af625db58cf2dc9bcc8d169c41bc24c
awesomeHiro <hrak.iha@gmail.com>
2020-05-02 20:08:39 +0900

    up

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,17 @@
 import colors from 'vuetify/es5/util/colors'
 
+// only add `router.base = '/<repository-name>/'` if `DEPLOY_ENV` is `GH_PAGES`
+const routerBase =
+  process.env.DEPLOY_ENV === 'GH_PAGES'
+    ? {
+        router: {
+          base: '/<repository-name>/',
+        },
+      }
+    : {}
+
 export default {
+  ...routerBase,
   mode: 'spa',
   /*
    ** Headers of the page
