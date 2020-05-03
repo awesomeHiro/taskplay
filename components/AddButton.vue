26c251d36b5fece7bfd82101627e9cb3a206067a
awesomeHiro <hrak.iha@gmail.com>
2020-05-03 14:15:38 +0900

    up

--- components/AddButton.vue
+++ components/AddButton.vue
@@ -1,15 +1,16 @@
 <template>
-  <v-btn color="pink" dark fixed bottom right fab @click="add">
+  <v-btn
+    color="pink"
+    fixed
+    bottom
+    right
+    @click="$store.tasks.dispatch('addTask', { name: 'new' })"
+  >
     <v-icon>mdi-plus</v-icon>
   </v-btn>
 </template>
 <script>
-import { mapActions } from 'vuex'
 export default {
-  methods: {
-    ...mapActions({
-      add: 'addtask',
-    }),
-  },
+  methods: {},
 }
 </script>
