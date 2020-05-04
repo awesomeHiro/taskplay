2f4b87364cb2e68eed83d064af2df0af10696479
awesomeHiro <hrak.iha@gmail.com>
2020-05-04 17:18:30 +0900

    asdfa

--- components/AddButton.vue
+++ components/AddButton.vue
@@ -7,31 +7,6 @@
         </v-btn>
       </template>
       <v-card class="mx-auto" max-width="374">
-        <v-card-title>Cafe Badilico</v-card-title>
-        <v-card-text>
-          <v-row align="center" class="mx-0">
-            <v-rating
-              :value="4.5"
-              color="amber"
-              dense
-              half-increments
-              readonly
-              size="14"
-            ></v-rating>
-
-            <div class="grey--text ml-4">4.5 (413)</div>
-          </v-row>
-
-          <div class="my-4 subtitle-1">
-            $ • Italian, Cafe
-          </div>
-
-          <div>
-            Small plates, salads & sandwiches - an intimate setting with 12
-            indoor seats plus patio seating.
-          </div>
-        </v-card-text>
-        <v-divider class="mx-4"></v-divider>
         <v-card-text>
           <v-chip-group
             v-model="project"
