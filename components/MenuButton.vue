c368b363e91d751ff267b88a9c9b84b950c0f746
awesomeHiro <hrak.iha@gmail.com>
2020-05-04 15:22:03 +0900

    bottom sheet

--- components/MenuButton.vue
+++ components/MenuButton.vue
@@ -1,14 +1,44 @@
 <template>
-  <v-btn fixed bottom left @click="menu">
-    <v-icon>mdi-menu</v-icon>
-  </v-btn>
+  <div class="text-center">
+    <v-bottom-sheet v-model="sheet">
+      <template v-slot:activator="{ on }">
+        <v-btn fixed bottom left v-on="on">
+          <v-icon>mdi-menu</v-icon>
+        </v-btn>
+      </template>
+      <v-list>
+        <v-list-item
+          v-for="tile in tiles"
+          :key="tile.title"
+          @click="sheet = false"
+        >
+          <v-list-item-avatar>
+            <v-avatar size="32px" tile>
+              <img
+                :src="
+                  `https://cdn.vuetifyjs.com/images/bottom-sheets/${tile.img}`
+                "
+                :alt="tile.title"
+              />
+            </v-avatar>
+          </v-list-item-avatar>
+          <v-list-item-title>{{ tile.title }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-bottom-sheet>
+  </div>
 </template>
 <script>
 export default {
-  methods: {
-    menu() {
-      alert('menu')
-    },
-  },
+  data: () => ({
+    sheet: false,
+    tiles: [
+      { img: 'keep.png', title: 'Sections' },
+      { img: 'inbox.png', title: 'Sets' },
+      { img: 'hangouts.png', title: 'Routines' },
+      { img: 'messenger.png', title: 'Projects' },
+      { img: 'google.png', title: 'Settings' },
+    ],
+  }),
 }
 </script>
