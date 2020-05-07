6a78c3517a128c26902258703d863ed2978cffcb
awesomeHiro <hrak.iha@gmail.com>
2020-05-08 08:14:22 +0900

    aa

--- pages/main.vue
+++ pages/main.vue
@@ -1,9 +1,9 @@
 <template>
   <v-row align="center" justify="center" class="caption" no-gutters>
     <v-col align="center" justify="center">
-      <v-card max-width="600px">
+      <v-card id="main" max-width="600px">
+        <Topbar />
         <v-divider></v-divider>
-
         <div class="text-center barely--text">April</div>
         <v-divider></v-divider>
         <daytab />
@@ -20,6 +20,7 @@
   </v-row>
 </template>
 <script>
+import Topbar from '~/components/Topbar.vue'
 import AddButton from '~/components/AddButton.vue'
 import MenuButton from '~/components/MenuButton.vue'
 import TimeSummary from '~/components/TimeSummary.vue'
@@ -28,6 +29,7 @@ import Tasklist from '~/components/Tasklist.vue'
 import Clock from '~/components/Clock.vue'
 export default {
   components: {
+    Topbar,
     AddButton,
     MenuButton,
     TimeSummary,
