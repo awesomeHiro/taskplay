0033621cd3672e8c9e7c6b87e4368c8ee227c46d
awesomeHiro <hrak.iha@gmail.com>
2020-05-20 08:12:05 +0900

    i tried firebase that was good but not need right now

--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,8 @@
 <script>
 // import Comp from '~/components/Progress.vue'
 import Comp from '~/pages/main.vue'
+// import Comp from '~/components/firebase/addFieldToFirestoreExample.vue'
+// import Comp from '~/components/firebase/addUserExample.vue'
 
 export default {
   components: {
