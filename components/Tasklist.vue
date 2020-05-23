da94a1e800abb23906833bd987c9eefc3fa9e81d
awesomeHiro <hrak.iha@gmail.com>
2020-05-23 19:26:18 +0900

    drag handle

--- components/Tasklist.vue
+++ components/Tasklist.vue
@@ -9,73 +9,75 @@
           </v-col>
           <v-col><v-divider clsss="ma-2"/></v-col>
         </v-row>
-        <draggable />
-        <v-list-item
-          v-for="(t, ti) in $store.getters['tasks/bySectionId'](s.id)"
-          :key="t.id"
-          class="pl-0 pr-0"
-        >
-          <v-col cols="1" class="pa-0 ma-0">
-            <div class="drag-bar pa-0 ma-0">
-              <v-icon color="barely">
-                drag_handle
-              </v-icon>
-            </div>
-          </v-col>
-          <v-col cols="1" class="pa-0 ma-0">
-            <div class="drag-bar pa-0 ma-0 subtle--text">
-              <span>
-                {{ $store.getters['sections/byId'](t.sectionId).name }}
-              </span>
-            </div>
-          </v-col>
-          <v-col cols="7" class="text-left pa-0">
-            <v-list-item-content class="pa-0">
-              <v-list-item-title
-                class="subtitle-2 subtle--text"
-                v-text="t.repeat ? t.name + ' ↺' : t.name"
-              ></v-list-item-title>
-              <v-list-item-subtitle
-                class="barely--text"
-                v-text="$store.getters['projects/byId'](t.projectId).name"
-              ></v-list-item-subtitle>
-              <v-list-item-subtitle
-                class="barely--text"
-                v-text="t.sortToken.slice(0, 3)"
-              ></v-list-item-subtitle>
-            </v-list-item-content>
-          </v-col>
-          <v-col cols="1" class="pa-0 ma-0">
-            <div class="barely--text">
-              {{ t.estimate }}
-            </div>
-            <div v-if="t.result" class="subtle--text">
-              {{ t.result }}
-            </div>
-          </v-col>
-          <v-col cols="1" class="pa-0 ma-0">
-            <!-- eslint-disable-next-line prettier/prettier -->
-                <div  v-if="t.result" :class="0 >= t.result - t.estimate  ? 'success--text' : 'error--text'">
+        <draggable handle=".handle">
+          <v-list-item
+            v-for="(t, ti) in $store.getters['tasks/bySectionId'](s.id)"
+            :key="t.id"
+            :disabled="Boolean(t.start)"
+            class="pl-0 pr-0"
+          >
+            <v-col cols="1" class="pa-0 ma-0">
+              <div class="handle pa-0 ma-0">
+                <v-icon color="barely">
+                  drag_handle
+                </v-icon>
+              </div>
+            </v-col>
+            <v-col cols="1" class="pa-0 ma-0">
+              <div class="pa-0 ma-0 subtle--text">
+                <span>
+                  {{ $store.getters['sections/byId'](t.sectionId).name }}
+                </span>
+              </div>
+            </v-col>
+            <v-col cols="7" class="text-left pa-0">
+              <v-list-item-content class="pa-0">
+                <v-list-item-title
+                  class="subtitle-2 subtle--text"
+                  v-text="t.repeat ? t.name + ' ↺' : t.name"
+                ></v-list-item-title>
+                <v-list-item-subtitle
+                  class="barely--text"
+                  v-text="$store.getters['projects/byId'](t.projectId).name"
+                ></v-list-item-subtitle>
+                <v-list-item-subtitle
+                  class="barely--text"
+                  v-text="t.sortToken.slice(0, 3)"
+                ></v-list-item-subtitle>
+              </v-list-item-content>
+            </v-col>
+            <v-col cols="1" class="pa-0 ma-0">
+              <div class="barely--text">
+                {{ t.estimate }}
+              </div>
+              <div v-if="t.result" class="subtle--text">
+                {{ t.result }}
+              </div>
+            </v-col>
+            <v-col cols="1" class="pa-0 ma-0">
               <!-- eslint-disable-next-line prettier/prettier -->
+                <div  v-if="t.result" :class="0 >= t.result - t.estimate  ? 'success--text' : 'error--text'">
+                <!-- eslint-disable-next-line prettier/prettier -->
                   {{
-                0 >= t.result - t.estimate
-                  ? t.result - t.estimate
-                  : '+' + (t.result - t.estimate)
-              }}
-            </div>
-          </v-col>
-          <v-col cols="1" class="ma-0 pa-0">
-            <div v-if="!ti" class="subtle--text">
-              {{ t.start }}
-            </div>
-            <div v-if="t.end" class="subtle--text">
-              {{ t.end }}
-            </div>
-            <div v-if="!t.end" class="barely--text">
-              {{ t.estFinishAt }}
-            </div>
-          </v-col>
-        </v-list-item>
+                  0 >= t.result - t.estimate
+                    ? t.result - t.estimate
+                    : '+' + (t.result - t.estimate)
+                }}
+              </div>
+            </v-col>
+            <v-col cols="1" class="ma-0 pa-0">
+              <div v-if="!ti" class="subtle--text">
+                {{ t.start }}
+              </div>
+              <div v-if="t.end" class="subtle--text">
+                {{ t.end }}
+              </div>
+              <div v-if="!t.end" class="barely--text">
+                {{ t.estFinishAt }}
+              </div>
+            </v-col>
+          </v-list-item>
+        </draggable>
         <v-row align="center" justify="center" no-gutters>
           <v-col cols="10">
             <Summary
