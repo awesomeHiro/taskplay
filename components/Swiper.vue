3085cc0fda0e427a2d6cb581022443186f5e10d5
awesomeHiro <hrak.iha@gmail.com>
2020-05-26 07:37:17 +0900

    up

---
+++ components/Swiper.vue
@@ -0,0 +1,49 @@
+<template>
+  <swiper ref="swiper" class="swiper" :options="swiperOption">
+    <swiper-slide class="menu">Menu slide</swiper-slide>
+    <swiper-slide class="content">
+      <div
+        class="menu-button"
+        :class="{ opened: menuOpened }"
+        @click="toggleMenu($event)"
+      >
+        <div class="bar"></div>
+        <div class="bar"></div>
+        <div class="bar"></div>
+      </div>
+      Content slide
+    </swiper-slide>
+  </swiper>
+</template>
+
+<script>
+export default {
+  name: 'SwiperExampleSlideableNavigationDrawer',
+  title: 'Slideable navigation drawer',
+  data() {
+    return {
+      menuOpened: false,
+      swiperOption: {
+        initialSlide: 1,
+        resistanceRatio: 0,
+        slidesPerView: 'auto',
+        on: {
+          slideChange: () => {
+            console.log(this.$refs.swiper.$swiper.activeIndex)
+          },
+        },
+      },
+    }
+  },
+  computed: {
+    swiper() {
+      return this.$refs.swiper.$swiper
+    },
+  },
+  methods: {
+    toggleMenu(event) {
+      this.menuOpened ? this.swiper.slideNext() : this.swiper.slidePrev()
+    },
+  },
+}
+</script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -47,7 +47,7 @@ export default {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: [],
+  plugins: [{ src: '~plugins/vue-awesome-swiper', ssr: false }],
   /*
    ** Nuxt.js dev-modules
    */
