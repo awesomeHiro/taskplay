7235bd63aa203f2a0349e22e96a09b18dc0f5e4d
awesomeHiro <hrak.iha@gmail.com>
2020-05-25 10:06:49 +0900

    up

--- pages/center.vue
+++ pages/center.vue
@@ -2,26 +2,18 @@
   <v-row align="center" justify="center" class="caption" no-gutters>
     <v-col align="center" justify="center">
       <v-card id="main" max-width="600px">
-        <v-divider />
-        <v-divider />
-        <Summary :tasks="this.$store.state.tasks.today" />
         <clock />
         <Todo :section="selectedSection" />
         <v-list-item />
-        <MenuButton />
       </v-card>
     </v-col>
   </v-row>
 </template>
 <script>
-import MenuButton from '~/components/MenuButton.vue'
-import Summary from '~/components/Summary.vue'
 import Todo from '~/components/Todo.vue'
 import Clock from '~/components/Clock.vue'
 export default {
   components: {
-    MenuButton,
-    Summary,
     Todo,
     Clock,
   },
