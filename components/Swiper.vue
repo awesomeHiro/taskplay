f661e9517a9c5d187b408d17228639757d09947a
awesomeHiro <hrak.iha@gmail.com>
2020-05-26 07:37:48 +0900

    up

--- components/Swiper.vue
+++ components/Swiper.vue
@@ -1,6 +1,6 @@
 <template>
   <swiper ref="swiper" class="swiper" :options="swiperOption">
-    <swiper-slide class="menu">Menu slide</swiper-slide>
+    <swiper-slide class="menu"><left /></swiper-slide>
     <swiper-slide class="content">
       <div
         class="menu-button"
@@ -11,15 +11,26 @@
         <div class="bar"></div>
         <div class="bar"></div>
       </div>
-      Content slide
+      <center />
     </swiper-slide>
+    <swiper-slide class="menu"><right /></swiper-slide>
   </swiper>
 </template>
 
 <script>
+import { Swiper, SwiperSlide } from 'vue-awesome-swiper'
+import 'swiper/css/swiper.css'
+import left from '~/pages/left.vue'
+import center from '~/pages/center.vue'
+import right from '~/pages/right.vue'
 export default {
-  name: 'SwiperExampleSlideableNavigationDrawer',
-  title: 'Slideable navigation drawer',
+  components: {
+    left,
+    center,
+    right,
+    Swiper,
+    SwiperSlide,
+  },
   data() {
     return {
       menuOpened: false,
@@ -29,7 +40,7 @@ export default {
         slidesPerView: 'auto',
         on: {
           slideChange: () => {
-            console.log(this.$refs.swiper.$swiper.activeIndex)
+            // this.menuOpened = this.swiper.activeIndex === 0
           },
         },
       },
@@ -47,3 +58,84 @@ export default {
   },
 }
 </script>
+
+<style lang="scss" scoped>
+@import './base.scss';
+.swiper {
+  .menu {
+    min-width: 100px;
+    width: 90%;
+    // max-width: 320px;
+    // background-color: #2c8dfb !important;
+    color: #fff;
+  }
+
+  .content {
+    width: 100%;
+  }
+
+  .menu-button {
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    padding: 15px;
+    cursor: pointer;
+    transition: 0.3s;
+    // background-color: #2c8dfb;
+
+    .bar {
+      position: relative;
+      display: block;
+      width: 50px;
+      height: 5px;
+      margin: 10px auto;
+      background-color: #fff;
+      border-radius: 10px;
+      transition: 0.3s;
+
+      &:nth-of-type(1) {
+        margin-top: 0px;
+      }
+      &:nth-of-type(3) {
+        margin-bottom: 0px;
+      }
+    }
+
+    &:hover {
+      .bar:nth-of-type(1) {
+        transform: translateY(1.5px) rotate(-4.5deg);
+      }
+      .bar:nth-of-type(2) {
+        opacity: 0.9;
+      }
+      .bar:nth-of-type(3) {
+        transform: translateY(-1.5px) rotate(4.5deg);
+      }
+    }
+
+    &.opened {
+      .bar:nth-of-type(1) {
+        transform: translateY(15px) rotate(-45deg);
+      }
+      .bar:nth-of-type(2) {
+        opacity: 0;
+      }
+      .bar:nth-of-type(3) {
+        transform: translateY(-15px) rotate(45deg);
+      }
+
+      &:hover {
+        .bar:nth-of-type(1) {
+          transform: translateY(13.5px) rotate(-40.5deg);
+        }
+        .bar:nth-of-type(2) {
+          opacity: 0.1;
+        }
+        .bar:nth-of-type(3) {
+          transform: translateY(-13.5px) rotate(40.5deg);
+        }
+      }
+    }
+  }
+}
+</style>
