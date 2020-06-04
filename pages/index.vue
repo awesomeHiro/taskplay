b26a87ab537e5eabd9bf8c416e1cd33c759530d0
awesomeHiro <hrak.iha@gmail.com>
2020-06-04 09:05:21 +0900

    refactor: tasklist finished

--- pages/index.vue
+++ pages/index.vue
@@ -5,16 +5,7 @@
 </template>
 
 <script>
-// import Comp from '~/components/Progress.vue'
-// import Comp from '~/pages/main.vue'
-// import Comp from '~/components/leftDrawer.vue'
-// import Comp from '~/pages/Swiper.vue'
-// import Comp from '~/components/Tasklist.vue'
-// import Comp from '~/pages/left.vue'
-import Comp from '~/pages/center.vue'
-// import Comp from '~/components/tmp.vue'
-// import Comp from '~/components/firebase/addFieldToFirestoreExample.vue'
-// import Comp from '~/components/firebase/addUserExample.vue'
+import Comp from '~/pages/main.vue'
 
 export default {
   components: {
